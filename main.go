cedc5f27a0165ebcd4ecb3302a1fee785ead0133
KariakiCode <kariakichernocherno@gmail.com>
2022-10-25 09:52:36 +0100

    first commit

--- main.go
+++ main.go
@@ -5,42 +5,20 @@ import (
 	"math/rand"
 	"time"
 )
-
-func userGuess(Guess int){
-	var user_Guess int;
-	var guess_count uint = 5;
-	for (user_Guess != Guess) && guess_count != 0 {
-		fmt.Println("Guess a number from [1 - 100]")
-		fmt.Scan(&user_Guess);
-		if user_Guess < Guess{
-			fmt.Println("too small")
-			guess_count--;
-			fmt.Printf("live remaining %v\n", guess_count)
-		}
-		if user_Guess > Guess {
-			fmt.Println("too big")
-			fmt.Printf("live reamining %v\n", guess_count)
-			guess_count--;
-		}
-		if guess_count == 0 {
-			var input string; 
-			fmt.Printf("Try again? y for yes || n for no: ")
-			fmt.Scan(&input)
-			if input == "y"{
-				guess_count = 5
-				continue
-			}else{
-				fmt.Println("Game over")
-				return
-			}
-		}
-	}
+func role_dice() (int, int){
+	x1 := rand.NewSource(time.Now().UnixNano())
+	y1 := rand.New(x1)
+	randn_1 := y1.Intn(6) + 1
+	randn_2 := y1.Intn(6) + 1
 	
-	fmt.Printf("yah you got the number: %v\n", Guess)
+	return randn_1, randn_2
 }
 func main(){
-    x1 := rand.NewSource(time.Now().UnixNano())
-	y1 := rand.New(x1)
-	Guess := y1.Intn(100) + 1
-	userGuess(Guess)
+	var rand_1 int;
+	var randn_2 int;
+	for i := 0; i < 1000; i++{
+		time.Sleep(time.Second)
+		rand_1, randn_2 = role_dice()
+		fmt.Printf("(%v, %v)\n", rand_1, randn_2)
+	}
 }
